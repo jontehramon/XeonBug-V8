0a04669800c3c9be0977bb1fc92a61afd2005ac5
jontehramon <153674534+jontehramon@users.noreply.github.com>
2024-09-29 11:10:05 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,19 +10,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "0713075619"
+global.ownername = "JONTEHRAMON"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
-global.location = "Pakistan, Karachi, Karachi"
+global.location = "Kenya, Nairobi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
-global.botname = 'XeonBug-V8' //name of the bot
+global.ownernumber = '713075619'  //creator number
+global.ownername = 'RAMON //owner name
+global.botname = 'XeonRAMONBug-V8' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'GlobalTech\n\nContact: +923444844060'
+global.author = 'RAMONTech\n\nContact: +254713075619'
 
 //console view/theme
 global.themeemoji = '🪀'
